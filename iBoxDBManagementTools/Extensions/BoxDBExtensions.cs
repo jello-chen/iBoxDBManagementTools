a2606d3018a04e0d2dd235d5c4b8fdfb7a88c284
陈金良 <jello_chen@live.com>
2019-06-19 23:26:01 +0800

    improve some where

---
+++ iBoxDBManagementTools/Extensions/BoxDBExtensions.cs
@@ -0,0 +1,38 @@
+﻿using iBoxDB.LocalServer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace iBoxDBManagementTools.Extensions
+{
+    internal static class BoxDBExtensions
+    {
+        public static DataTable ToDataTable(this List<Local> locals)
+        {
+            if (locals == null)
+                throw new ArgumentNullException(nameof(locals));
+
+            var dt = new DataTable();
+            if(locals.Count > 0)
+            {
+                for (int i = 0; i < locals.Count; i++)
+                {
+                    var dr = dt.NewRow();
+                    var local = locals[i];
+                    for (int j = 0; j < local.Count; j++)
+                    {
+                        if (i == 0 && j == 0)
+                        {
+                            Console.WriteLine(local.Entity.TableName);
+                            dt.Columns.AddRange(local.Keys.Select(k => new DataColumn(k)).ToArray());
+                        }
+                        dr[j] = local.Values.ElementAt(j);
+                    }
+                    dt.Rows.Add(dr);
+                }
+            }
+            return dt;
+        }
+    }
+}
---
+++ iBoxDBManagementTools/MainWindow.xaml.cs
@@ -0,0 +1,101 @@
+﻿using iBoxDB.LocalServer;
+using iBoxDBManagementTools.Extensions;
+using System;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace iBoxDBManagementTools
+{
+    public partial class MainWindow : Window
+    {
+        private DB server;
+        private DB.AutoBox db;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (server?.IsClosed() == false)
+                server.Close();
+        }
+
+        private void TextBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            FolderBrowserDialog directoryDialog = new FolderBrowserDialog();
+            directoryDialog.RootFolder = Environment.SpecialFolder.DesktopDirectory;
+            if(directoryDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                this.tbFilePath.Text = directoryDialog.SelectedPath;
+            }
+        }
+
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(this.tbFilePath.Text))
+            {
+                System.Windows.MessageBox.Show("Please select iBoxDB file", "Warning");
+                return;
+            }
+            if (db == null)
+            {
+                server = new DB(this.tbFilePath.Text);
+                db = server.Open();
+            }
+            var tables = db.GetDatabase().GetSchemata().Keys;
+            foreach (string table in tables)
+            {
+                if (!table.StartsWith("__"))
+                    this.cbTable.Items.Add(table);
+            }
+        }
+
+        private void CbTable_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) 
+            => this.tbSql.Text = $"from {e.AddedItems[0]}";
+
+        private void BtnExecute_Click(object sender, RoutedEventArgs e)
+        {
+            var sql = this.tbSql.SelectedText;
+            if (string.IsNullOrWhiteSpace(sql))
+            {
+                sql = this.tbSql.Text;
+            }
+
+            if (string.IsNullOrWhiteSpace(sql))
+            {
+                System.Windows.MessageBox.Show("Please input your querying sentence", "Warning");
+                return;
+            }
+
+            if (server == null || server.IsClosed())
+            {
+                System.Windows.MessageBox.Show("Please connect before querying", "Warning");
+                return;
+            }
+
+            var ql = string.Empty;
+            var args = new string[0];
+            var ci = sql.IndexOf(',');
+            if(ci != -1)
+            {
+                ql = sql.Substring(0..ci);
+                if(ci < sql.Length - 1)
+                {
+                    args = sql.Substring(ci + 1).Trim().Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+                }
+            }
+            else
+            {
+                ql = sql;
+            }
+
+            var locals = db.Select(ql, args);
+            var dt = locals.ToDataTable();
+            this.dg.ItemsSource = dt.DefaultView;
+        }
+    }
+}
